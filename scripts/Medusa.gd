691885cb38ee848e523d3e667c5d3807f87a7455
Josselin <julien.josselin75@hotmail.fr>
2021-09-18 18:42:09 +0200

    Clean and draft Medusa.gd

--- scripts/Medusa.gd
+++ scripts/Medusa.gd
@@ -1,28 +1,35 @@
 extends KinematicBody
 
-# var chasing = false
 
-##########################
-# move randomly (pathfinding?)
-# (whithout falling)
+# TODO
+# required :
+# x follow player
+# - detect player (far/near)
+# - follow when detected for x sec
+# - kill when eyes meet
+# later :
+# - move randomly / stroll
+# - spawn randomly
 
-# _physics_process(delta): -> if chasing == false:
-# func chose_a_direction()
-#	avoid obstacles and void
-#	
-# 	func walk_for(rand_sec)
-# 		func dodge_obstacles_and_void
-# 		func turn_if_obstacles
-# 	func wait(rand_sec)
 
+#####
+# => Follow Player Vars
 # Godot uses multiple points (nodes) for a path.
 var path = [] # to store nodes
 var path_node = 0 # keep path on which node we're currently on
 var speed = 12
 onready var nav = get_parent() # nav mesh that contains the whole world geometry
 onready var player = $"../../../Player"
+#####
+
+# var chasing = false
+
 
 func _physics_process(delta):
+	follow_player()
+
+
+func follow_player():
 	if path_node < path.size():
 		var direction = (path[path_node] - global_transform.origin)
 		if direction.length() < 1:
@@ -37,6 +44,19 @@ func move_to(target_pos):
 func _on_Timer_timeout():
 	move_to(player.global_transform.origin) # location of player
 
+
+##########################
+# func move_randomly
+# (whithout falling)
+
+# _physics_process(delta): -> if chasing == false:
+# func chose_a_direction()
+#	avoid obstacles and void
+#	
+# 	func walk_for(rand_sec)
+# 		func dodge_obstacles_and_void
+# 		func turn_if_obstacles
+# 	func wait(rand_sec)
 
 ##########################
 # detect player
