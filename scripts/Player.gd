c8360f38c9b21f92dff302296ad70011a4bce78b
Josselin <julien.josselin75@hotmail.fr>
2021-09-18 16:46:54 +0200

    Player moves

---
+++ scripts/Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody
+
+
+export var speed = 14
+export var gravity = 75
+var velocity = Vector3.ZERO
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+
+	# turn character
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+
+	# apply movement
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	velocity = move_and_slide(velocity, Vector3.UP)
+	# gravity
+	velocity.y -= gravity * delta
