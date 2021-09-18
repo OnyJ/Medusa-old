d8e55d65cdf78526ed505f7b1cab5680b04d724b
Josselin <julien.josselin75@hotmail.fr>
2021-09-18 18:21:05 +0200

    Medusa follows player

--- scripts/Medusa.gd
+++ scripts/Medusa.gd
@@ -15,6 +15,28 @@ extends KinematicBody
 # 		func turn_if_obstacles
 # 	func wait(rand_sec)
 
+# Godot uses multiple points (nodes) for a path.
+var path = [] # to store nodes
+var path_node = 0 # keep path on which node we're currently on
+var speed = 12
+onready var nav = get_parent() # nav mesh that contains the whole world geometry
+onready var player = $"../../../Player"
+
+func _physics_process(delta):
+	if path_node < path.size():
+		var direction = (path[path_node] - global_transform.origin)
+		if direction.length() < 1:
+			path_node += 1
+		else:
+			move_and_slide(direction.normalized() * speed, Vector3.UP)
+
+func move_to(target_pos):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
+	path_node = 0
+
+func _on_Timer_timeout():
+	move_to(player.global_transform.origin) # location of player
+
 
 ##########################
 # detect player
@@ -39,3 +61,5 @@ extends KinematicBody
 
 ##########################
 # (spawn randomly)
+
+
