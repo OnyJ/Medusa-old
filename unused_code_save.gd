8f8fdfbab60103ce0c2baa84e10f7ea742d8cf3b
Josselin <julien.josselin75@hotmail.fr>
2021-09-19 11:39:56 +0200

    save unused code

---
+++ unused_code_save.gd
@@ -0,0 +1,6 @@
+### func LOOK AT PLAYER
+#	look_at_from_position(global_transform.origin, player_position, Vector3.UP)
+#	look_at(player_position, Vector3.UP)
+#	self.rotate(Vector3.UP, rotation.y)
+#	self.rotate_y()
+#	direction = velocity.rotated(Vector3.UP, rotation.y)
